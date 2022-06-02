e961323ba6866cf0554457c2fa18bcb797357355
4ClassStuff <60152699+4ClassStuff@users.noreply.github.com>
2022-06-02 17:22:16 -0400

    User credential model

--- src/main/java/Models/UserCredentials.java
+++ src/main/java/Models/UserCredentials.java
@@ -1,9 +1,70 @@
 package Models;
 
+import java.util.Objects;
+
 public class UserCredentials {
 	private int id;
 	private String username;
 	private String password;
+	
+	public UserCredentials() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserCredentials(int id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserCredentials [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, password, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserCredentials other = (UserCredentials) obj;
+		return id == other.id && Objects.equals(password, other.password) && Objects.equals(username, other.username);
+	}
+	
 	
 	
 }
